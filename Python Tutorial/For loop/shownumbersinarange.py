30d11d3d36238b504d83c5c0079971dace70a896
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2024-07-15 01:34:22 +0800

    Create shownumbersinarange.py

---
+++ Python Tutorial/For loop/shownumbersinarange.py
@@ -0,0 +1,11 @@
+
+# this script is to print numbers between 1-100
+
+print("This program is to print all the even numbers between 1 to 100")
+
+for num in range(0,100,2):
+    print(num)
+
+#print all the numnbers between 1 - 100
+for num1 in range(1,100):
+    print(num1)
