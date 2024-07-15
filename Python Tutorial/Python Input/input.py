1f8421ede3668364aacb64e3368e0bb6f8ab125a
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2024-07-15 13:43:22 +0800

    Create input.py

---
+++ Python Tutorial/Python Input/input.py
@@ -0,0 +1,18 @@
+
+
+# type in python
+
+# program to check input
+
+num = int(input("Please enter your number here:"))
+
+print(num)
+
+name1 = input("please enter your name here:")
+
+print(name1)
+
+print("type of number:", type(num))
+
+print("type of name:", type(name1))
+
