acbeb0148ef46b8721a479eb44c3a31963fc586c
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2024-07-15 13:53:22 +0800

    Create different_data_type.py

---
+++ Python Tutorial/Python Input/different_data_type.py
@@ -0,0 +1,22 @@
+
+
+#taking input as string
+
+color = input("what color is rose?:")
+
+print(color)
+
+#Taking input as int
+#typecasting to int
+
+n = int(input("How many roses do you have:"))
+
+print(n)
+
+#taking input as float
+#Typecasting to float
+
+price = float(input("Price of each rose:"))
+print(price)
+
+
