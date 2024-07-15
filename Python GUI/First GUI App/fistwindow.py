10ebacd2fe83809a9bc2e476fff86f6a01d029cf
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2024-07-15 16:00:02 +0800

    Create fistwindow.py

---
+++ Python GUI/First GUI App/fistwindow.py
@@ -0,0 +1,16 @@
+
+#Created on 15th Jul 2025
+#Author: pengchao
+
+
+#import gui module tkinter
+import tkinter as tk
+
+#create instance
+win = tk.Tk()
+
+#Add a tittle
+win.title("Pengchao GUI")
+
+#start the GUI
+win.mainloop()
