80481e6ac32a0fc6be58107221626f2e32c0ecaf
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2024-07-15 14:05:20 +0800

    Create listinput.py

---
+++ Python Tutorial/Python Input/listinput.py
@@ -0,0 +1,15 @@
+
+
+
+#Taking list1 input from user as list
+
+list1 = list(input("Please engter elements of list1:"))
+
+#Taking list2 input from user as list
+
+list2 = list(input("Please enter elements of list2:"))
+
+for i in list2:
+    list1.append(i)
+
+print(list1)
