2a59bc0bf8eebb50bf9cd373e840da4510f491d0
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2024-07-15 13:47:15 +0800

    Create floatnum.py

---
+++ Python Tutorial/Python Input/floatnum.py
@@ -0,0 +1,20 @@
+
+
+
+
+# this script is to exerise the data type 
+
+num = int(input("Please type your number here:"))
+
+print(num)
+
+print("the type of number is:", type(num))
+
+
+floatnum = float(input("Please type your float number here:"))
+
+print(floatnum)
+
+print("The type of number is:", type(floatnum))
+
+
