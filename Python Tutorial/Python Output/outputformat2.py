248dc9023f4b870f6b705438723ba03c4eefaaf3
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2024-07-15 15:03:28 +0800

    Create outputformat2.py

---
+++ Python Tutorial/Python Output/outputformat2.py
@@ -0,0 +1,13 @@
+cstr = "I love DevOps"
+
+# Printing the center aligned string with fillchr
+print("Center aligned string with fillchr: ")
+print(cstr.center(40, '&'))
+
+# Printing the left aligned string with "-" padding
+print("The left aligned string is : ")
+print(cstr.ljust(40, '-'))
+
+# Printing the right aligned string with "-" padding
+print("The right aligned string is : ")
+print(cstr.rjust(40, '-'))
