fcab9b8360874af990eb2479c75bb7d72c719a85
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2024-07-15 14:00:02 +0800

    Create plustwonumbers.py

---
+++ Python Tutorial/Python Input/plustwonumbers.py
@@ -0,0 +1,14 @@
+
+
+#Taking number 1 from user as int
+
+num1 = int(input("Please enter first number here:"))
+
+# Taking number 2 from user as int
+
+num2 = int(input("Please enter second number here:"))
+
+addition = num1 + num2
+
+print("The sum of the two given numbers is {}".format(addition))
+
