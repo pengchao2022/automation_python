9e50f48c1461e4fd772a3ad7d29bf8026a52f9c9
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2024-07-15 14:26:06 +0800

    Create output2.py

---
+++ Python Tutorial/Python Output/output2.py
@@ -0,0 +1,17 @@
+
+
+
+
+print("hellowrld \n is everybody's first c program!")
+
+a,b,=10,1000
+
+print("the value of a is {} and b is {}".format(a,b))
+
+n = input("Please enter your number here:")
+
+print("Number entered by user:",n)
+
+print(type(n))
+
+
