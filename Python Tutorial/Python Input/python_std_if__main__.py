25d22c94c6a4dd59b49d7dc729b15c606848ce71
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2024-07-15 15:36:58 +0800

    Create python_std_if__main__.py

---
+++ Python Tutorial/Python Input/python_std_if__main__.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    num1 = int(input("Please type your first number here:"))
+    num2 = int(input("Please type your second number here:"))
+
+    result = num1 + num2
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
